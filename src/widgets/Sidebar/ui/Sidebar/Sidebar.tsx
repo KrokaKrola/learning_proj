a5931ec70fc9909d7dc4b16df37e18cb3088e221
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-06-08 21:27:19 +0300

    comment failed code

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,18 +3,17 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Sidebar.module.scss';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed] = useState(false);
 
-  const onToggle = () => {
-    setCollapsed(!collapsed);
-  };
+  // const onToggle = () => {
+  //   setCollapsed(!collapsed);
+  // };
 
   return (
     <aside
@@ -23,13 +22,13 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
       ])}
       data-testid="Sidebar"
     >
-      <Button
-        theme={ThemeButton.CLEAR}
-        data-testid="Sidebar-toggle"
-        onClick={onToggle}
-      >
-        toggle
-      </Button>
+      {/* <Button */}
+      {/*  theme={ThemeButton.CLEAR} */}
+      {/*  data-testid="Sidebar-toggle" */}
+      {/*  onClick={onToggle} */}
+      {/* > */}
+      {/*  toggle */}
+      {/* </Button> */}
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher />

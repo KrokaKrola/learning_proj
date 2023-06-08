e6651833a942a0aa943585fe4ea93ef818526899
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-06-08 21:29:47 +0300

    add toggle text

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,17 +3,20 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Sidebar.module.scss';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
-  const [collapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation();
 
-  // const onToggle = () => {
-  //   setCollapsed(!collapsed);
-  // };
+  const onToggle = () => {
+    setCollapsed(!collapsed);
+  };
 
   return (
     <aside
@@ -22,13 +25,13 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
       ])}
       data-testid="Sidebar"
     >
-      {/* <Button */}
-      {/*  theme={ThemeButton.CLEAR} */}
-      {/*  data-testid="Sidebar-toggle" */}
-      {/*  onClick={onToggle} */}
-      {/* > */}
-      {/*  toggle */}
-      {/* </Button> */}
+      <Button
+        theme={ThemeButton.CLEAR}
+        data-testid="Sidebar-toggle"
+        onClick={onToggle}
+      >
+        {t('toggle')}
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher />

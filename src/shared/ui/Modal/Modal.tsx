d096e68a8fb891a30058809d700c7b8dbad3ee4b
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-06-13 21:13:18 +0300

    update theme names and passing to modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -69,7 +69,7 @@ export const Modal: FC<ModalProps> = (props) => {
 
   return (
     <Portal>
-      <div className={classNames(cls.Modal, mods, [className, cls[theme]])}>
+      <div className={classNames(cls.Modal, mods, [className, theme])}>
         <div className={cls.overlay} onClick={handleClose}>
           <div className={cls.content} onClick={onContentClick}>
             {children}

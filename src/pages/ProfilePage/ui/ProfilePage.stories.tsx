4059140fa68681a57e849399626b54fb6900d533
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-06-24 11:06:02 +0300

    update screenshots

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,11 +2,25 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import ProfilePage from './ProfilePage';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { profileReducer } from 'entities/Profile';
 
 const meta = {
   title: 'pages/ProfilePage',
   component: ProfilePage,
   argTypes: {},
+  decorators: [
+    (Story) => (
+      <StoreDecorator
+        initialState={{}}
+        asyncReducers={{
+          profile: profileReducer,
+        }}
+      >
+        <Story />
+      </StoreDecorator>
+    ),
+  ],
 } satisfies Meta<typeof ProfilePage>;
 
 export default meta;

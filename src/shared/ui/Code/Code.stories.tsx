f951fdb59856f360e14031b844087110ec252fbe
Frontend_Oleh_K <oleh.kotlinskyi@abz.agency>
2023-07-09 22:39:01 +0300

    update code stories

--- src/shared/ui/Code/Code.stories.tsx
+++ src/shared/ui/Code/Code.stories.tsx
@@ -12,7 +12,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    children:
-      '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
+    text: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
   },
 };
